f1882c0ea75f4804aaa9106e8859ea72e6202958
Dinesh Venkatachalam <dineshraj.venkatachalam@hotmail.com>
2018-02-08 21:48:54 -0500

    Added student module

---
+++ SchoolUI/src/app/student/student.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddStudentComponent } from './add-student/add-student.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AddStudentComponent]
+})
+export class StudentModule { }
