7091e267abc3235afa24951b1235b70fb3c1029a
Dinesh <dineshraj.venkatachalam@hotmail.com>
2018-02-19 20:06:24 -0500

    Referenced ng-bootstrap

--- SchoolUI/src/app/app.component.ts
+++ SchoolUI/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+private router: Router;
+
+  onTabChange($event: NgbTabChangeEvent) {
+    if ($event.nextId === 'first') {
+      this.router.navigateByUrl('/addStudent');
+    } else if ($event.nextId === 'second') {
+      this.router.navigateByUrl('/addStudent');
+    }
+  }
 }
--- SchoolUI/src/app/app.module.ts
+++ SchoolUI/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AddStudentComponent } from './student/add-student/add-student.component';
 import { GetStudentsComponent } from './student/get-students/get-students.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { GetStudentsComponent } from './student/get-students/get-students.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
